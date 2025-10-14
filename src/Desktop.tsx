c5845e7fda497a645339e537e4cde19b3638b484
Cindy Zhang <xinyuz62@uci.edu>
2025-10-14 12:07:57 -0700

    add load circle

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import defaultBackground from "./images/default_background.jpeg";
 import "./Desktop.css"
+import { CircularProgress } from "@mui/material";
 
 function Desktop() {
     // const [screenHeight,setScreenHeight] = useState(window.innerHeight)
@@ -12,6 +13,7 @@ function Desktop() {
 
     const sleep = (ms:number) => new Promise(resolve => setTimeout(resolve, ms))
 
+    const [showLoad, setShowLoad] = useState(false)
     const [showAnimation1, setShowAnimation1] = useState(false)
     const [showAnimation2, setShowAnimation2] = useState(false)
 
@@ -22,7 +24,9 @@ function Desktop() {
     const [animation1Counter, setAnimation1Counter] = useState(0)
     const handleCloseAnimation1 = async () => {
         setShowAnimation1(false)
+        setShowLoad(true)
         await sleep(1000)
+        setShowLoad(false)
         if (animation1Counter+1 >= dialog1Options.length) {
             setAnimation1Counter(0)
             setShowAnimation2(true)
@@ -40,7 +44,8 @@ function Desktop() {
             border: "1px solid black", 
             borderRadius:"5px",
             background: "linear-gradient(to bottom, #8faacd, #d2def1ff)",
-            boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)"}}>
+            boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)",
+            marginTop:"auto"}}>
             <div style={{display:"flex",flexDirection:"row"}}>
                 <div style={{
                     width:"100%",
@@ -61,7 +66,14 @@ function Desktop() {
                     background:"linear-gradient(to bottom, #ffdedeff, #ab5151ff)",
                     color:"white"}} onClick={() => handleCloseAnimation1()}>X</div>
             </div>
-            <div style={{border:"1px solid grey",display:"flex",justifyContent: "center",marginTop:"10px",height:"200px",width:"350px",background:"white"}}>
+            <div style={{
+                border:"1px solid grey",
+                display:"flex",
+                justifyContent: "center",
+                marginTop:"10px",
+                height:"200px",
+                width:"350px",
+                background:"white"}}>
                 <div style={{
                     width:"100%",
                     paddingLeft:"10px",
@@ -69,7 +81,13 @@ function Desktop() {
                     textAlign:"left",
                     fontFamily:"'Lucida Console', monospace",
                     overflowWrap: "break-word"}}>{dialog1Options[animation1Counter]}</div>
-                <button onClick={() => handleCloseAnimation1()} style={{position:"absolute",height:"fit-content",bottom:"20px",boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.2)",cursor:"pointer"}}>Ok</button>
+                <button onClick={() => handleCloseAnimation1()} 
+                        style={{
+                            position:"absolute",
+                            height:"fit-content",
+                            bottom:"20px",
+                            boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.2)",
+                            cursor:"pointer"}}>Ok</button>
             </div>
         </dialog>
     }
@@ -89,7 +107,10 @@ function Desktop() {
         <div id="desktop-div" style={{
             backgroundImage: `url(${backgroundImage})`, 
             backgroundSize: 'cover', 
-            backgroundPosition: 'center'}}>
+            backgroundPosition: 'center',
+            display:"flex",
+            flexDirection:"column",
+            justifyContent: "center"}}>
             <button style={{
                 background:"none",
                 border:"none",
@@ -99,6 +120,7 @@ function Desktop() {
                 <img src={require("./images/gift.png")} alt='gift box' style={{width:'150px',cursor:'pointer'}} onClick={triggerAnimation}/>
             </button>
             <Animation1/>
+            {showLoad && <CircularProgress style={{margin:"auto"}}/>}
         </div>
     )
 }
